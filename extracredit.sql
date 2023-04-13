ba07de877a2dfcae3c6bfbc80ed163729a5ad383
aronaacho <114690424+aronaacho@users.noreply.github.com>
2023-04-13 15:38:50 -0700

    Adding data

--- extracredit.sql
+++ extracredit.sql
@@ -1,5 +1,9 @@
 -- creating timeslots table
 CREATE TABLE timeslots (start TIME, end TIME, id PRIMARY KEY);
+INSERT INTO timeslots VALUES 
+('12:00:00', '13:00:00', 1),
+('14:00:00', '15:30:00', 2),
+('09:00:00', '11:00:00', 3);
 
 -- creating alternations to rooms table by creating and renaming temp table
 CREATE TABLE temprooms (number INTEGER, buildingid REFERENCES buildings(id), seating INTEGER, id INTEGER PRIMARY KEY);
@@ -9,3 +13,8 @@ ALTER TABLE temprooms RENAME TO rooms;
 
 -- creating schedule table
 CREATE TABLE schedule (coursecode, roomid, timeslotid, UNIQUE(roomid, timeslotid));
+INSERT INTO schedule VALUES 
+('CMS303B', 1, 1),
+('INFO201A', 2, 3),
+('CSE122B', 4, 3),
+('INFO330A', 2, 4);
